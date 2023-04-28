c034626fb1b2dbb4cff0e9038b0034527148f055
Brian Pugh <bnp117@gmail.com>
2023-04-28 11:37:54 -0700

    test min_pattern_size

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -24,7 +24,7 @@ Our custom huffman size table:
 import io
 import unittest
 
-from tamp import Compressor, ExcessBitsError, bit_size
+from tamp import Compressor, ExcessBitsError, bit_size, compute_min_pattern_size
 
 
 class TestCompressorHelpers(unittest.TestCase):
@@ -33,6 +33,14 @@ class TestCompressorHelpers(unittest.TestCase):
 
     def test_bit_size_excess(self):
         self.assertEqual(bit_size(1 << 32), -1)
+
+    def test_min_pattern_size(self):
+        self.assertEqual(compute_min_pattern_size(window=10, literal=8), 2)
+        self.assertEqual(compute_min_pattern_size(window=15, literal=5), 3)
+
+    def test_min_pattern_size_out_of_range(self):
+        with self.assertRaises(ValueError):
+            compute_min_pattern_size(0, 0)
 
 
 class TestCompressor(unittest.TestCase):
