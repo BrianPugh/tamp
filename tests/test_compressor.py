64e237fa2cbe6d459a9c1a80c60bf73025508d2d
Brian Pugh <bnp117@gmail.com>
2023-04-28 12:11:33 -0700

    single-shot text compress test

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -24,7 +24,13 @@ Our custom huffman size table:
 import io
 import unittest
 
-from tamp import Compressor, ExcessBitsError, bit_size, compute_min_pattern_size
+from tamp import (
+    Compressor,
+    ExcessBitsError,
+    bit_size,
+    compress,
+    compute_min_pattern_size,
+)
 
 
 class TestCompressorHelpers(unittest.TestCase):
@@ -200,3 +206,23 @@ class TestCompressor(unittest.TestCase):
             with self.assertRaises(ExcessBitsError):
                 compressor.write(b"\xFF")
                 compressor.flush()
+
+    def test_single_shot_compress_text(self):
+        expected = bytes(
+            # fmt: off
+            [
+                0b010_11_0_0_0,  # header (window_bits=10, literal_bits=8)
+                0b1_0110011,    # literal "f"
+                0b0_0_0_00100,  # the pre-init buffer contains "oo" at index 131
+                                # size=2 -> 0b0
+                                # 131 -> 0b0010000011
+                0b00011_1_00,   # literal " "
+                0b100000_0_1,   # There is now "foo " at index 0
+                0b000_00000,    # size=4 -> 0b1000
+                0b00000_0_11,   # Just "foo" at index 0; size=3 -> 0b11
+                0b00000000,     # index 0 -> 0b0000000000
+                0b00_000000,    # 6 bits of zero-padding
+            ]
+            # fmt: on
+        )
+        self.assertEqual(compress("foo foo foo"), expected)
