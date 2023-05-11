4758eb1a36182aea0ed62f4cf3a90c3df39f478f
Brian Pugh <bnp117@gmail.com>
2023-05-11 12:12:02 -0700

    more debugging info

--- tools/c-profiler/main.c
+++ tools/c-profiler/main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include "tamp/compressor.h"
 
 #define EXIT(x, fmt, ...) { ret_code=x; fprintf(stderr, fmt, ##__VA_ARGS__); goto exit; }
 
 int main(int argc, char *argv[]) {
     int ret_code = 0;
-    FILE *file = NULL;
+    FILE *uncompressed_file = NULL;
     unsigned char *uncompressed_data = NULL;
+    FILE *compressed_file = NULL;
     unsigned char *compressed_data = NULL;
-    size_t file_len;
-    size_t compressed_len;
+    size_t uncompressed_file_len;
+    size_t compressed_len=0;
 
     TampCompressor compressor;
     TampConf compressor_conf = {.literal=8, .window=10, .use_custom_dictionary=false};
@@ -19,36 +21,43 @@ int main(int argc, char *argv[]) {
     if (!(compressor_buffer = malloc(1 << compressor_conf.window)))
         EXIT(1, "OOM");
 
-    if (!(file = fopen("../../build/enwik8", "rb")))
-        EXIT(1, "Unable to open file %s", "build/enwik8");
+    if (!(uncompressed_file = fopen("../../build/enwik8", "rb")))
+        EXIT(1, "Unable to open uncompressed_file %s", "build/enwik8");
+    if (!(compressed_file = fopen("output.tamp", "wb")))
+        EXIT(1, "Unable to open compressed_file");
 
-    fseek(file, 0, SEEK_END);
-    file_len = ftell(file);
-    fseek(file, 0, SEEK_SET);
+    fseek(uncompressed_file, 0, SEEK_END);
+    uncompressed_file_len = ftell(uncompressed_file);
+    fseek(uncompressed_file, 0, SEEK_SET);
 
-    if (!(uncompressed_data = malloc(file_len)))
+    if (!(uncompressed_data = malloc(uncompressed_file_len)))
         EXIT(1, "OOM");
 
-    if (!(compressed_data = malloc(file_len)))
+    if (!(compressed_data = malloc(uncompressed_file_len)))
         EXIT(1, "OOM");
 
-    fread(uncompressed_data, file_len, 1, file);
+    fread(uncompressed_data, uncompressed_file_len, 1, uncompressed_file);
+    printf("Uncompressed Length: %zu\n", uncompressed_file_len);
 
     if(TAMP_OK != tamp_compressor_init(&compressor, &compressor_conf, compressor_buffer))
         EXIT(1, "Failed to initialize compressor");
 
     printf("Beginning compressing...\n");
-    if(TAMP_OK != tamp_compressor_compress(
+    size_t consumed;
+    if(TAMP_OK != tamp_compressor_compress_and_flush(
                 &compressor,
                 compressed_data,
-                file_len,
+                uncompressed_file_len,
                 &compressed_len,
                 uncompressed_data,
-                file_len,
-                NULL
+                uncompressed_file_len,
+                &consumed
                 ))
         EXIT(1, "Failed to compress data");
 
+    fwrite(compressed_data, 1, compressed_len, compressed_file);
+
+    printf("Consumed: %zu\n", consumed);
     printf("Compressed Length: %zu\n", compressed_len);
 
 exit:
@@ -58,8 +67,10 @@ exit:
         free(compressed_data);
     if(compressor_buffer)
         free(compressor_buffer);
-    if(file)
-        fclose(file);
+    if(uncompressed_file)
+        fclose(uncompressed_file);
+    if(compressed_file)
+        fclose(compressed_file);
 
     return ret_code;
 }
