aa2d352092c8499214caaf8612ab43818c4f4729
Brian Pugh <bnp117@gmail.com>
2024-04-22 15:13:18 -0700

    Add reference readinto implementation

--- tests/test_decompressor.py
+++ tests/test_decompressor.py
@@ -186,3 +186,34 @@ class TestDecompressor(unittest.TestCase):
 
                 read1 = decompressor.read()
                 self.assertEqual(read0 + read1, expected)
+
+    def test_decompressor_readinto(self):
+        for Decompressor in Decompressors:
+            with self.subTest(Decompressor=Decompressor):
+                expected = b"foo foo foo"
+
+                compressed = bytes(
+                    # fmt: off
+                    [
+                        0b010_11_00_0,  # header (window_bits=10, literal_bits=8)
+                        0b1_0110011,    # literal "f"
+                        0b0_0_0_00100,  # the pre-init buffer contains "oo" at index 131
+                                        # size=2 -> 0b0
+                                        # 131 -> 0b0010000011
+                        0b00011_1_00,   # literal " "
+                        0b100000_0_1,   # There is now "foo " at index 0
+                        0b000_00000,    # size=4 -> 0b1000
+                        0b00000_0_11,   # Just "foo" at index 0; size=3 -> 0b11
+                        0b00000000,     # index 0 -> 0b0000000000
+                        0b00_000000,    # 6 bits of zero-padding
+                    ]
+                    # fmt: on
+                )
+
+                actual_buf = bytearray(1024)
+                with BytesIO(compressed) as f:
+                    decompressor = Decompressor(f)
+                    actual_len = decompressor.readinto(actual_buf)
+
+                self.assertEqual(actual_len, len(expected))
+                self.assertEqual(actual_buf[:actual_len], expected)
