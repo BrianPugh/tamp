4c37d9284647fe47db798ef93608d2f401f9383a
Brian Pugh <bnp117@gmail.com>
2024-04-24 14:07:45 -0700

    simplify test_pseudorandom; add native module test

--- tests/test_pseudorandom.py
+++ tests/test_pseudorandom.py
@@ -4,10 +4,26 @@ import tamp.compressor
 import tamp.decompressor
 from tamp import initialize_dictionary
 
+modules = [tamp.compressor, tamp.decompressor]
+
 try:
     import micropython
 except ImportError:
     micropython = None
+
+if micropython:
+    import tamp.compressor_viper
+    import tamp.decompressor_viper
+
+    modules.append(tamp.compressor_viper)
+    modules.append(tamp.decompressor_viper)
+
+    try:
+        import tamp_native
+
+        modules.append(tamp_native)
+    except ImportError:
+        pass
 
 
 class TestPseudoRandom(unittest.TestCase):
@@ -17,43 +33,15 @@ class TestPseudoRandom(unittest.TestCase):
     def test_256_compressor_nonzero_seed(self):
         self.assertNotEqual(initialize_dictionary(256, seed=1), bytearray(256))
 
-    def test_256_compressor(self):
-        actual = tamp.compressor.initialize_dictionary(256)
-        self.assertEqual(len(actual), 256)
-        self.assertEqual(
-            actual,
-            b"\x00.//r.0. t>\n/>snas.trnr i\x00r/a\x00snat./.r\x00i o.s tneo>.as>\na.ta\x00 aa\x00\x00\x000oe ri\x00a>eatsi\n.\ni.str\n//snesr.ost<  \x00\ni\neoa\x00se0.o\n\n>aori>n0.>./.oonen0<\x00<r o\n\naas0< ai\n0\x00na\x00e><.\noas to \n></se>>ts/oreatinter.n0 >s\n/.e.><. r si<>/<san\x00ae t 0.r.o/0./a r/ttn nn.<re.t0 \x00r\x00ro",
-        )
-
-    def test_256_decompressor(self):
-        actual = tamp.decompressor.initialize_dictionary(256)
-        self.assertEqual(len(actual), 256)
-        self.assertEqual(
-            actual,
-            b"\x00.//r.0. t>\n/>snas.trnr i\x00r/a\x00snat./.r\x00i o.s tneo>.as>\na.ta\x00 aa\x00\x00\x000oe ri\x00a>eatsi\n.\ni.str\n//snesr.ost<  \x00\ni\neoa\x00se0.o\n\n>aori>n0.>./.oonen0<\x00<r o\n\naas0< ai\n0\x00na\x00e><.\noas to \n></se>>ts/oreatinter.n0 >s\n/.e.><. r si<>/<san\x00ae t 0.r.o/0./a r/ttn nn.<re.t0 \x00r\x00ro",
-        )
-
-    @unittest.skipIf(micropython is None, "not running micropython")
-    def test_256_compressor_viper(self):
-        import tamp.compressor_viper
-
-        actual = tamp.compressor_viper.initialize_dictionary(256)
-        self.assertEqual(len(actual), 256)
-        self.assertEqual(
-            actual,
-            b"\x00.//r.0. t>\n/>snas.trnr i\x00r/a\x00snat./.r\x00i o.s tneo>.as>\na.ta\x00 aa\x00\x00\x000oe ri\x00a>eatsi\n.\ni.str\n//snesr.ost<  \x00\ni\neoa\x00se0.o\n\n>aori>n0.>./.oonen0<\x00<r o\n\naas0< ai\n0\x00na\x00e><.\noas to \n></se>>ts/oreatinter.n0 >s\n/.e.><. r si<>/<san\x00ae t 0.r.o/0./a r/ttn nn.<re.t0 \x00r\x00ro",
-        )
-
-    @unittest.skipIf(micropython is None, "not running micropython")
-    def test_256_decompressor_viper(self):
-        import tamp.decompressor_viper
-
-        actual = tamp.decompressor_viper.initialize_dictionary(256)
-        self.assertEqual(len(actual), 256)
-        self.assertEqual(
-            actual,
-            b"\x00.//r.0. t>\n/>snas.trnr i\x00r/a\x00snat./.r\x00i o.s tneo>.as>\na.ta\x00 aa\x00\x00\x000oe ri\x00a>eatsi\n.\ni.str\n//snesr.ost<  \x00\ni\neoa\x00se0.o\n\n>aori>n0.>./.oonen0<\x00<r o\n\naas0< ai\n0\x00na\x00e><.\noas to \n></se>>ts/oreatinter.n0 >s\n/.e.><. r si<>/<san\x00ae t 0.r.o/0./a r/ttn nn.<re.t0 \x00r\x00ro",
-        )
+    def test_256_compressor_int(self):
+        for module in modules:
+            with self.subTest(module=module):
+                actual = module.initialize_dictionary(256)
+                self.assertEqual(len(actual), 256)
+                self.assertEqual(
+                    actual,
+                    b"\x00.//r.0. t>\n/>snas.trnr i\x00r/a\x00snat./.r\x00i o.s tneo>.as>\na.ta\x00 aa\x00\x00\x000oe ri\x00a>eatsi\n.\ni.str\n//snesr.ost<  \x00\ni\neoa\x00se0.o\n\n>aori>n0.>./.oonen0<\x00<r o\n\naas0< ai\n0\x00na\x00e><.\noas to \n></se>>ts/oreatinter.n0 >s\n/.e.><. r si<>/<san\x00ae t 0.r.o/0./a r/ttn nn.<re.t0 \x00r\x00ro",
+                )
 
 
 if __name__ == "__main__":
