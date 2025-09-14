BrianPugh/tamp

commit 85ef612bd424e767b6f46af9199a48e015221b0c

decompressor progress

tamp/_c_src/tamp/decompressor.c
@@ -1,8 +1,69 @@
+#include <string.h>
 #include "decompressor.h"
 #include "common.h"
 
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+#define FLUSH 127
+
+/**
+ * @brief Decode a huffman match-size symbol from the decompressor's bit_buffer.
+ *
+ * Internally updates bit_buffer and bit_buffer_pos.
+ * In the event of TAMP_INPUT_EXHAUSTED, bit_buffer is NOT restored.
+ *
+ * @return Tamp Status Code.
+ *     TAMP_INVALID_SYMBOL if no valid symbol decoded. Buffer is NOT restored.
+ */
+static int8_t huffman_decode(TampDecompressor *decompressor){
+    uint8_t code = 0;
+
+    for(uint8_t i=0; i < 8; i++){
+        if(decompressor->bit_buffer_pos == 0)
+            return TAMP_INPUT_EXHAUSTED;
+        code = (code << 1) | (decompressor->bit_buffer >> 31);
+        decompressor->bit_buffer <<= 1;
+        decompressor->bit_buffer_pos -= 1;
+        switch(code){
+            case 0:
+                return 0;
+            case 3:
+                return 1;
+            case 8:
+                return 2;
+            case 11:
+                return 3;
+            case 20:
+                return 4;
+            case 36:
+                return 5;
+            case 38:
+                return 6;
+            case 43:
+                return 7;
+            case 75:
+                return 8;
+            case 84:
+                return 9;
+            case 148:
+                return 10;
+            case 149:
+                return 11;
+            case 170:
+                return 12;
+            case 39:
+                return 13;
+            case 171:
+                return FLUSH;
+        }
+    }
+    return TAMP_INVALID_SYMBOL;
+}
 
 tamp_res tamp_decompressor_read_header(TampConf *conf, const unsigned char *input, size_t input_size, size_t *input_consumed_size) {
+    if(input_consumed_size)
+        (*input_consumed_size) = 0;
     if(input_size == 0)
         return TAMP_INPUT_EXHAUSTED;
     if(input[0] & 0x1)
@@ -13,9 +74,31 @@ tamp_res tamp_decompressor_read_header(TampConf *conf, const unsigned char *inpu
     conf->window = ((input[0] >> 5) & 0x7) + 8;
     conf->literal = ((input[0] >> 3) & 0x3) + 5;
     conf->use_custom_dictionary = ((input[0] >> 2) & 0x1);
+
+    return TAMP_OK;
 }
 
-tamp_res tamp_decompressor_init(TampDecompressor decompressor, const TampConf *conf, unsigned char *window){
+tamp_res tamp_decompressor_init(TampDecompressor *decompressor, const TampConf *conf, unsigned char *window){
+    decompressor->window = window;
+    decompressor->bit_buffer = 0;
+    decompressor->bit_buffer_pos = 0;
+    decompressor->skip_bytes = 0;
+
+    if(conf){
+        if( conf->window < 8 || conf->window > 15)
+            return TAMP_INVALID_CONF;
+        if( conf->literal < 5 || conf->literal > 8)
+            return TAMP_INVALID_CONF;
+
+        decompressor->min_pattern_size = tamp_compute_min_pattern_size(conf->window, conf->literal);
+        memcpy(&decompressor->conf, conf, sizeof(TampConf));
+        decompressor->configured = true;
+    }
+    else{
+        // Defer configuration to tamp_decompressor_decompress
+        decompressor->configured = false;
+    }
+    return TAMP_OK;
 }
 
 tamp_res tamp_decompressor_decompress(
@@ -27,4 +110,97 @@ tamp_res tamp_decompressor_decompress(
         size_t input_size,
         size_t *input_consumed_size
         ){
+    tamp_res res;
+    const uint16_t window_mask = (1 << decompressor->conf.window) - 1;
+
+    if(!decompressor->configured){
+        //Read in header
+        size_t header_consumed_size;
+        res = tamp_decompressor_read_header(&decompressor->conf, input, input_size, &header_consumed_size);
+        if(res != TAMP_OK)
+            return res;
+        input_size -= header_consumed_size;
+        input++;
+        if(input_consumed_size)
+            (*input_consumed_size) += header_consumed_size;
+    }
+    while(input_size || decompressor->bit_buffer_pos){
+        int8_t match_size = 1;
+        // Populate the bit buffer
+        while(input_size && decompressor->bit_buffer_pos <= 24){
+            decompressor->bit_buffer_pos += 8;
+            decompressor->bit_buffer |=  *input << (32 - decompressor->bit_buffer_pos);
+            input_size--;
+            input++;
+            if(input_consumed_size)
+                (*input_consumed_size)++;
+        }
+
+        if(output_size == 0)
+            return TAMP_OUTPUT_FULL;
+
+        if(decompressor->bit_buffer >> 31){
+            // is literal
+            if(decompressor->bit_buffer_pos < (1 + decompressor->conf.literal))
+                return TAMP_INPUT_EXHAUSTED;
+            decompressor->bit_buffer <<= 1;  // shift out the is_literal flag
+
+            // Copy literal to output
+            *output = decompressor->bit_buffer >> (32 - decompressor->conf.literal);
+            decompressor->bit_buffer <<= decompressor->conf.literal;
+            decompressor->bit_buffer_pos -= decompressor->conf.literal;
+        }
+        else{
+            // is token; attempt a decode
+            uint32_t bit_buffer_backup = decompressor->bit_buffer;
+            uint8_t bit_buffer_pos_backup = decompressor->bit_buffer_pos;
+            uint16_t window_offset;
+
+            decompressor->bit_buffer <<= 1;  // shift out the is_literal flag
+
+            match_size = huffman_decode(decompressor);
+            if(match_size < 0){
+                decompressor->bit_buffer = bit_buffer_backup;
+                decompressor->bit_buffer_pos = bit_buffer_pos_backup;
+                return match_size;
+            }
+            if(decompressor->bit_buffer_pos < decompressor->conf.window){
+                decompressor->bit_buffer = bit_buffer_backup;
+                decompressor->bit_buffer_pos = bit_buffer_pos_backup;
+                return TAMP_INPUT_EXHAUSTED;
+            }
+            window_offset = decompressor->bit_buffer >> (32 - decompressor->conf.window);
+            decompressor->bit_buffer <<= decompressor->conf.window;
+            decompressor->bit_buffer_pos -= decompressor->conf.window;
+
+            // Apply previous skip_bytes
+            match_size -= decompressor->skip_bytes;
+            window_offset += decompressor->skip_bytes;
+
+            // Check if we are output-buffer-limited, and if so to set skip_bytes and restore the bit_buffer
+            if(match_size > output_size){
+                decompressor->skip_bytes += output_size;
+                match_size = output_size;
+                decompressor->bit_buffer = bit_buffer_backup;
+                decompressor->bit_buffer_pos = bit_buffer_pos_backup;
+            }
+            else {
+                decompressor->skip_bytes = 0;
+            }
+
+            // Copy pattern to output
+            for(uint8_t i=0; i < match_size; i++){
+                output[i] = decompressor->window[window_offset + i];
+            }
+        }
+        // Copy pattern from output to current window position
+        for(uint8_t i=0; i < match_size; i++){
+            decompressor->window[decompressor->window_pos] = output[i];
+            decompressor->window_pos = (decompressor->window_pos + 1) & window_mask;
+        }
+
+        output += match_size;
+        output_size -= match_size;
+    }
+    return TAMP_OK;
 }

tamp/_c_src/tamp/common.h
@@ -13,11 +13,12 @@ typedef enum {
     /* Normal/Recoverable status >= 0 */
     TAMP_OK = 0,
     TAMP_OUTPUT_FULL = 1,  // Wasn't able to complete action due to full output buffer.
-    TAMP_INPUT_EXHAUSTED = 2, // Wasn't able to complete action due to exhausted input buffer.
 
     /* Error codes < 0 */
     TAMP_EXCESS_BITS = -1,  // Provided symbol has more bits than conf->literal
-    TAMP_INVALID_CONF = -2, //
+    TAMP_INVALID_CONF = -2,  // Invalid configuration parameters.
+    TAMP_INPUT_EXHAUSTED = -3, // Wasn't able to complete action due to exhausted input buffer.
+    TAMP_INVALID_SYMBOL = -4,  // Unknown huffman code encountered.
 } tamp_res;
 
 typedef struct TampConf {

tamp/_c_src/tamp/compressor.c
@@ -195,7 +195,6 @@ tamp_res tamp_compressor_init(TampCompressor *compressor, const TampConf *conf, 
         conf = &conf_default;
     if( conf->window < 8 || conf->window > 15)
         return TAMP_INVALID_CONF;
-
     if( conf->literal < 5 || conf->literal > 8)
         return TAMP_INVALID_CONF;
 

tamp/_c_src/tamp/compressor.h
@@ -9,6 +9,7 @@ extern "C" {
 
 typedef struct TampCompressor{
     unsigned char *window;
+    TampConf conf;
     unsigned char input[16];
     uint32_t bit_buffer;
     uint32_t bit_buffer_pos:5;
@@ -16,7 +17,6 @@ typedef struct TampCompressor{
     uint32_t input_size:5;
     uint32_t input_pos:4;
     uint32_t window_pos:15;
-    TampConf conf;
 } TampCompressor;
 
 

tamp/_c_src/tamp/decompressor.h
@@ -9,9 +9,13 @@ extern "C" {
 
 typedef struct {
     unsigned char *window;
+    TampConf conf;
     uint32_t bit_buffer;
     uint32_t bit_buffer_pos:5;
     uint32_t min_pattern_size:2;
+    uint32_t configured:1;  // Whether or not conf has been properly set
+    uint32_t window_pos:15;
+    uint32_t skip_bytes:4;  // Skip this many decompressed bytes (from previous output-buffer-limited decompression).
 } TampDecompressor;
 
 /**
@@ -35,7 +39,7 @@ tamp_res tamp_decompressor_read_header(TampConf *conf, const unsigned char *inpu
  *                   If conf.use_custom_dictionary is true, then the window must be
  *                   externally initialized.
  */
-tamp_res tamp_decompressor_init(TampDecompressor decompressor, const TampConf *conf, unsigned char *window);
+tamp_res tamp_decompressor_init(TampDecompressor *decompressor, const TampConf *conf, unsigned char *window);
 
 /**
  * @brief

commit 4126c63113f183865cb67d296c16c553afbcfd5e

c: decompressor: Force integer promotion type for bit_buffer
C does automatic integer promotion when performing calculations with
smaller types (like unsigned integers). But it only guarantees the size
(and type) of an int. But the  bit_buffer is explicitly an uint32_t. It can
therefore be that an int is not 32 bit (depending on the target
architecture).

Also, shifting a signed integer (affecting the sign bit) is undefined
behavior in C. Compilers like GCC can therefore perform optimizations which
break the expected behavior. The shifted type must therefore be forced to
unsigned.

This can be seen when enabling the undefined behavior sanitizer of
GCC/clang:

    decompressor.c:135:43: runtime error: left shift of 131 by 24 places cannot be represented in type 'int'

Fixes: 85ef612bd424 ("decompressor progress")
Signed-off-by: Sven Eckelmann <sven@narfation.org>

tamp/_c_src/tamp/decompressor.c
@@ -131,8 +131,9 @@ tamp_res tamp_decompressor_decompress_cb(TampDecompressor *decompressor, unsigne
     while (input != input_end || decompressor->bit_buffer_pos) {
         // Populate the bit buffer
         while (input != input_end && decompressor->bit_buffer_pos <= 24) {
+            uint32_t t = *input;
             decompressor->bit_buffer_pos += 8;
-            decompressor->bit_buffer |= *input << (32 - decompressor->bit_buffer_pos);
+            decompressor->bit_buffer |= t << (32 - decompressor->bit_buffer_pos);
             input++;
             (*input_consumed_size)++;
         }